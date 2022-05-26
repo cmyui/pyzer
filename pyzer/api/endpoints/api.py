312f711196c56e2d09fabfc2b1b5e6e015c54b49
cmyui <cmyuiosu@gmail.com>
2022-05-25 20:36:54 -0400

    add stats table & many missing user fields to db

--- pyzer/api/endpoints/api.py
+++ pyzer/api/endpoints/api.py
@@ -41,44 +41,54 @@ async def get_current_user(session: models.Session = Depends(authorize_session))
     # https://osu.ppy.sh/docs/index.html#user
 
     # validate credentials
-    row = await services.database.fetch_one(
+    user_row = await services.database.fetch_one(
         "SELECT * FROM users WHERE id = :id",
         {"id": session["id"]},
     )
-    if row is None:
+    if user_row is None:
         return None  # TODO: what's correct error?
 
-    user_id = row["id"]
+    statistics_row = await services.database.fetch_one(
+        "SELECT * FROM user_statistics WHERE id = :id",
+        {"id": session["id"]},
+    )
+    if statistics_row is None:
+        return None  # TODO: what's correct error?
 
-    return {
+    user_id = user_row["id"]
+
+    resp = {
         "id": user_id,
         # TODO: customizable subdomains?
         "avatar_url": f"https://a.{settings.DOMAIN}/{user_id}.jpeg",
-        "country_code": row["country"],
+        "country_code": user_row["country"],
         "default_group": "default",  # TODO: whats this?
-        "is_active": True,
-        "is_bot": False,
-        "is_deleted": False,
+        "is_active": True,  # TODO: whats this?
+        "is_bot": user_row["is_bot"],
+        "is_deleted": False,  # TODO: account deletions/deactivations?
         "is_online": True,  # TODO: is this always true when calling this endpoint?
-        "is_supporter": True,  # TODO
-        "last_visit": "2020-01-01T00:00:00+00:00",
-        "pm_friends_only": row["pm_friends_only"],
-        "profile_colour": row["profile_colour"],
-        "username": row["username"],
+        "is_supporter": user_row["is_supporter"],
+        "last_visit": "2020-01-01T00:00:00+00:00",  # TODO
+        "pm_friends_only": user_row["pm_friends_only"],
+        "profile_colour": user_row["profile_colour"],
+        "username": user_row["username"],
         "cover_url": "https://assets.ppy.sh/user-profile-covers/1/0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef.jpeg",  # TODO
-        "discord": row["discord"],
-        "has_supported": True,  # TODO
-        "interests": None,  # TODO
-        "join_date": row["join_date"].isoformat(),  # "2010-01-01T00:00:00+00:00",
-        "kudosu": {"total": 0, "available": 0},  # TODO
-        "location": None,  # TODO
-        "max_blocks": row["max_blocks"],
-        "max_friends": row["max_friends"],
-        "occupation": None,  # TODO str?
+        "discord": user_row["discord"],
+        "has_supported": user_row["has_supported"],
+        "interests": user_row["interests"],  # NOTE: nullable
+        "join_date": user_row["join_date"].isoformat(),  # "2010-01-01T00:00:00+00:00",
+        "kudosu": {
+            "total": user_row["kudosu_total"],
+            "available": user_row["kudosu_available"],
+        },
+        "location": user_row["location"],  # NOTE: nullable
+        "max_blocks": user_row["max_blocks"],
+        "max_friends": user_row["max_friends"],
+        "occupation": user_row["occupation"],  # NOTE: nullable
         "playmode": "osu",  # TODO preference?
-        "playstyle": ["mouse", "touch"],  # TODO
+        "playstyle": ["mouse", "tablet", "keyboard", "touch"],  # TODO
         "post_count": 0,  # TODO?
-        "profile_order": [  # TODO
+        "profile_order": [  # TODO (pg json field?)
             "me",
             "recent_activity",
             "beatmaps",
@@ -87,16 +97,16 @@ async def get_current_user(session: models.Session = Depends(authorize_session))
             "top_ranks",
             "medals",
         ],
-        "title": None,  # TODO
-        "twitter": row["twitter"],
-        "website": row["website"],
+        "title": user_row["title"],  # TODO
+        "twitter": user_row["twitter"],
+        "website": user_row["website"],
         "country": {  # TODO, and also why is code duplicated
-            "code": row["country"],
-            "name": country_code_to_long_form(row["country"]),
+            "code": user_row["country"],
+            "name": country_code_to_long_form(user_row["country"]),
         },
         "cover": {  # TODO, and also why is code duplicated
-            "custom_url": "https://assets.ppy.sh/user-profile-covers/1/0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef.jpeg",
-            "url": "https://assets.ppy.sh/user-profile-covers/1/0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef.jpeg",
+            "custom_url": "https://x.catboy.best/21PPQcH.png",
+            "url": "https://x.catboy.best/21PPQcH.png",
             "id": None,
         },
         "is_restricted": False,  # TODO
@@ -111,7 +121,7 @@ async def get_current_user(session: models.Session = Depends(authorize_session))
             },
         ],
         "favourite_beatmapset_count": 0,  # TODO
-        "follower_count": row["follower_count"],
+        "follower_count": user_row["follower_count"],
         "graveyard_beatmapset_count": 0,  # TODO
         "groups": [  # TODO
             # {
@@ -142,21 +152,25 @@ async def get_current_user(session: models.Session = Depends(authorize_session))
             # {"start_date": "2020-01-01", "count": 1},
         ],
         "scores_first_count": 0,  # TDOO
-        "statistics": {  # TODO
-            "level": {"current": 60, "progress": 55},
-            "pp": 100,
-            "global_rank": 2000,
-            "ranked_score": 2000000,
-            "hit_accuracy": 90.5,
-            "play_count": 1000,
-            "play_time": 100000,
-            "total_score": 3000000,
-            "total_hits": 6000,
-            "maximum_combo": 500,
-            "replays_watched_by_others": 270,
-            "is_ranked": True,
+        "statistics": {
+            # TODO: level calculation based on total score
+            "level": {"current": 0, "progress": 0},  # progress 0-100
+            # TODO: if score.pp is also an int in the api,
+            # then we can just make it int everywhere
+            "pp": int(statistics_row["pp"]),
+            "global_rank": statistics_row["global_rank"],
+            "ranked_score": statistics_row["ranked_score"],
+            "hit_accuracy": statistics_row["hit_accuracy"],
+            "play_count": statistics_row["play_count"],
+            "play_time": statistics_row["play_time"],
+            "total_score": statistics_row["total_score"],
+            "total_hits": statistics_row["total_hits"],
+            "maximum_combo": statistics_row["maximum_combo"],
+            "replays_watched_by_others": statistics_row["replays_watched_by_others"],
+            "is_ranked": statistics_row["is_ranked"],
+            # TODO
             "grade_counts": {"ss": 10, "ssh": 5, "s": 50, "sh": 0, "a": 40},
-            "rank": {"global": 15000, "country": 30000},
+            "rank": {"global": 15000, "country": 30000},  # TODO: where is this used?
         },
         "support_level": 3,  # TODO: what's this?
         "user_achievements": [  # TODO
@@ -169,6 +183,9 @@ async def get_current_user(session: models.Session = Depends(authorize_session))
             ],
         },
     }
+
+    return resp
+
     # return {
     #     "id": 1,
     #     "join_date": "2010-01-01T00:00:00+00:00",  # "2020-01-01T00:00:00Z",
